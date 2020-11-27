e027a1836cda0291c7b48a55f3a1b3d4fe534bbe
Matt Reath <mattreath@icloud.com>
2020-11-27 11:45:39 -0600

    Fixed lint issues with Python code

--- betagraph.py
+++ betagraph.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 a1, b1 = 10, 10
-rv0 = beta(a1,b1)
+rv0 = beta(a1, b1)
 
 a2, b2 = 3, 3
-rv1 = beta(a2,b2)
+rv1 = beta(a2, b2)
 
-x = np.linspace(0,1,100)
-plt.plot(x,rv0.pdf(x))
-plt.plot(x,rv1.pdf(x))
+x = np.linspace(0, 1, 100)
+plt.plot(x, rv0.pdf(x))
+plt.plot(x, rv1.pdf(x))
 plt.show()
--- chapter5.py
+++ chapter5.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 # predefined win rates for each slot machine (AI model is unaware of these)
 conversion_rates = [0.10, 0.14, 0.05, 0.08, 0.12]
-reward_values = [1,1,1,1,1]
+reward_values = [1, 1, 1, 1, 1]
 
 N = 5000                   # number of samples
 d = len(conversion_rates)   # number of slot machines
@@ -29,7 +29,7 @@ for i in range(N):
     for j in range(d):
         # distribution graph for the slot machines will shift to the right for the best slot machine
         random_guess = np.random.beta(positive_reward[j] + 1, negative_reward[j] + 1)
-        #print(str(random_guess) + " : " + str(max_random))
+        # print(str(random_guess) + " : " + str(max_random))
         if random_guess > max_random:
             max_random = random_guess
             selected = j   
@@ -42,7 +42,7 @@ for i in range(N):
         negative_reward[selected] += 1
 
 # display the details of the results
-#print(positive_reward)
+# print(positive_reward)
 # print(negative_reward)
 # selected_machines = positive_reward + negative_reward
 # for i in range(d):
@@ -50,8 +50,8 @@ for i in range(N):
 # print('Conclusion: Best machine is machine number ' + str(np.argmax(selected_machines) + 1))
 print("\nRewards By Machine = ", positive_reward)
 print("\nNo Rewards By Machine = ", negative_reward)
-#print("\nTotal Rewards = ")
-#print("\nMachine Selected At Each Round : ", machine_selected)
+# print("\nTotal Rewards = ")
+# print("\nMachine Selected At Each Round : ", machine_selected)
 
 # plt.bar(['B1','B2','B3','B4','B5'],positive_reward)
 # plt.title('MABP')
@@ -70,18 +70,18 @@ print("\nNo Rewards By Machine = ", negative_reward)
 # plt.ylabel('No. Of Times Each Bandit Was Selected')
 # plt.show()
 
-rv0 = beta(positive_reward[0],negative_reward[0])
-rv1 = beta(positive_reward[1],negative_reward[1])
-rv2 = beta(positive_reward[2],negative_reward[2])
-rv3 = beta(positive_reward[3],negative_reward[3])
-rv4 = beta(positive_reward[4],negative_reward[4])
+rv0 = beta(positive_reward[0], negative_reward[0])
+rv1 = beta(positive_reward[1], negative_reward[1])
+rv2 = beta(positive_reward[2], negative_reward[2])
+rv3 = beta(positive_reward[3], negative_reward[3])
+rv4 = beta(positive_reward[4], negative_reward[4])
 
-x = np.linspace(0,.2,200)
+x = np.linspace(0, .2, 200)
 plt.title('Beta Distribution By Slot Machine')
-plt.plot(x,rv0.pdf(x), label="Machine A [" + str(round((positive_reward[0]/negative_reward[0])*100,1)) + "%]")
-plt.plot(x,rv1.pdf(x), label="Machine B [" + str(round((positive_reward[1]/negative_reward[1])*100,1)) + "%]")
-plt.plot(x,rv2.pdf(x), label="Machine C [" + str(round((positive_reward[2]/negative_reward[2])*100,1)) + "%]")
-plt.plot(x,rv3.pdf(x), label="Machine D [" + str(round((positive_reward[3]/negative_reward[3])*100,1)) + "%]")
-plt.plot(x,rv4.pdf(x), label="Machine E [" + str(round((positive_reward[4]/negative_reward[4])*100,1)) + "%]")
+plt.plot(x, rv0.pdf(x), label="Machine A [" + str(round((positive_reward[0]/negative_reward[0])*100,1)) + "%]")
+plt.plot(x, rv1.pdf(x), label="Machine B [" + str(round((positive_reward[1]/negative_reward[1])*100,1)) + "%]")
+plt.plot(x, rv2.pdf(x), label="Machine C [" + str(round((positive_reward[2]/negative_reward[2])*100,1)) + "%]")
+plt.plot(x, rv3.pdf(x), label="Machine D [" + str(round((positive_reward[3]/negative_reward[3])*100,1)) + "%]")
+plt.plot(x, rv4.pdf(x), label="Machine E [" + str(round((positive_reward[4]/negative_reward[4])*100,1)) + "%]")
 plt.legend()
 plt.show()
