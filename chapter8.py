9bb3a6ba4cd48593afecdaecad8cc170393945f2
Matt Reath <mattreath@icloud.com>
2020-12-04 12:40:07 -0600

    Added optional steps in chapter 8

--- chapter8.py
+++ chapter8.py
@@ -1,18 +1,6 @@
 import numpy as np
 
 
-def route(starting_location, ending_location):
-    best_route = [starting_location]
-    next_location = starting_location
-    while next_location != ending_location:
-        starting_state = location_to_state[starting_location]
-        route_next_state = np.argmax(Q[starting_state, ])
-        next_location = state_to_location[route_next_state]
-        best_route.append(next_location)
-        starting_location = next_location
-    return best_route
-
-
 # Set parameters for the Q-Learning
 gamma = 0.75
 alpha = 0.9
@@ -46,30 +34,46 @@ R = np.array([[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],             # A
               [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],             # D
               [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],             # E
               [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],             # F
-              [0, 0, 1, 0, 0, 0, 1000, 1, 0, 0, 0, 0],          # G
+              [0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0],          # G
               [0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1],             # H
               [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0],             # I
               [0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0],             # J
               [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],             # K
               [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0]])            # L
 
-# initialize Q-values to 0
-Q = np.array(np.zeros([12, 12]))
 
-# train until TD for Q-values is converged
-for i in range(1000):
-    current_state = np.random.randint(0, 12)
-    playable_actions = []
-    for j in range(12):
-        if R[current_state, j] > 0:
-            playable_actions.append(j)
-    next_state = np.random.choice(playable_actions)
-    TD = R[current_state, next_state] + gamma * Q[next_state, np.argmax(Q[next_state,])] - Q[current_state, next_state]
-    Q[current_state, next_state] = Q[current_state, next_state] + alpha * TD
+def route(starting_location, ending_location):
+    R_new = np.copy(R)
+    ending_state = location_to_state[ending_location]
+    R_new[ending_state, ending_state] = 1000
+    # initialize Q-values to 0
+    Q = np.array(np.zeros([12, 12]))
+    # train until TD for Q-values is converged
+    for i in range(1000):
+        current_state = np.random.randint(0, 12)
+        playable_actions = []
+        for j in range(12):
+            if R_new[current_state, j] > 0:
+                playable_actions.append(j)
+        next_state = np.random.choice(playable_actions)
+        TD = R_new[current_state, next_state] + gamma * Q[next_state, np.argmax(Q[next_state,])] - Q[
+            current_state, next_state]
+        Q[current_state, next_state] = Q[current_state, next_state] + alpha * TD
 
-print("Q-values:")
-print(Q.astype(int))
+    calculated_route = [starting_location]
+    next_location = starting_location
+    while next_location != ending_location:
+        starting_state = location_to_state[starting_location]
+        route_next_state = np.argmax(Q[starting_state, ])
+        next_location = state_to_location[route_next_state]
+        calculated_route.append(next_location)
+        starting_location = next_location
+    return calculated_route
+
+
+def best_route(starting_location, intermediate_location, ending_location):
+    return route(starting_location, intermediate_location) + route(intermediate_location, ending_location)[1:]
+
 
 # output the best route
-print('Route:')
-print(route('E', 'G'))
+print('\nRoute: ', best_route('E', 'K', 'G'))
